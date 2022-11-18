82d223573a5ba30c26979c1769bd31482a97128b
btxdev <limecompro@gmail.com>
2022-11-18 22:19:39 +0500

    Рабочий танк

--- hardware/master/master.ino
+++ hardware/master/master.ino
@@ -1,15 +1,109 @@
+#include <AmperkaServo.h>
+
+// servo settings
+#define LEFT_SERVO_PIN 6
+#define LEFT_SERVO_PIN_FB A14
+#define RIGHT_SERVO_PIN 7
+#define RIGHT_SERVO_PIN_FB A15
+#define MCS_MIN 500
+#define MCS_MAX 2500
+#define FEEDBACK_MIN 0.247
+#define FEEDBACK_MAX 3.063 
+
+const uint8_t SERVO_SPEED = 255;
+
+AmperkaServo leftServo;
+AmperkaServo rightServo;
+
+// state of buttons from WiFi App
+bool BTN_LEFT_FLAG = false;
+bool BTN_RIGHT_FLAG = false;
+bool BTN_TOP_FLAG = false;
+bool BTN_BOTTOM_FLAG = false;
 
 void setup() 
 { 
+  // debug
   Serial.begin(115200);
+  // wifi
   Serial1.begin(9600);
 
+  leftServo.attach(LEFT_SERVO_PIN, MCS_MIN, MCS_MAX);
+  leftServo.attachFB(LEFT_SERVO_PIN_FB,FEEDBACK_MIN, FEEDBACK_MAX);
+
+  rightServo.attach(RIGHT_SERVO_PIN, MCS_MIN, MCS_MAX);
+  rightServo.attachFB(RIGHT_SERVO_PIN_FB, FEEDBACK_MIN, FEEDBACK_MAX);
 } 
- 
+
+int looper = 0;
+
 void loop() 
 { 
+  // read codes from WiFI module
   if (Serial1.available()) {
-    int inByte = Serial1.read();
-    Serial.write(inByte);
+    uint8_t inByte = Serial1.read();
+    if (inByte == 1) BTN_LEFT_FLAG = true;
+    else if (inByte == 2) BTN_LEFT_FLAG = false;
+    else if (inByte == 3) BTN_RIGHT_FLAG = true;
+    else if (inByte == 4) BTN_RIGHT_FLAG = false;
+    else if (inByte == 5) BTN_TOP_FLAG = true;
+    else if (inByte == 6) BTN_TOP_FLAG = false;
+    else if (inByte == 7) BTN_BOTTOM_FLAG = true;
+    else if (inByte == 8) BTN_BOTTOM_FLAG = false;
+    else {
+      BTN_LEFT_FLAG = false;
+      BTN_RIGHT_FLAG = false;
+      BTN_TOP_FLAG = false;
+      BTN_BOTTOM_FLAG = false;
+    }
+    Serial.println(inByte);
+    // uint8_t btn = inByte / 10;
+    // uint8_t state = btn % 10;
+    // Serial.print(inByte);
+    // Serial.print(" ");
+    // Serial.print(btn);
+    // Serial.print(" ");
+    // Serial.print(state);
+    // Serial.println(";");
+    // if (inByte > 0 && inByte <= 4) SELECTED_BTN = inByte;
+    // bool state = inByte == 6;
+    // if(inByte == 5 || inByte == 6) {
+    //   if (SELECTED_BTN == 1) BTN_LEFT_FLAG = state;
+    //   if (SELECTED_BTN == 2) BTN_RIGHT_FLAG = state;
+    //   if (SELECTED_BTN == 3) BTN_TOP_FLAG = state;
+    //   if (SELECTED_BTN == 4) BTN_BOTTOM_FLAG = state;
+    //   // SELECTED_BTN = 0;
+    // }
+    // Serial.write(inByte);
   }
+
+  // looper++;
+  // if(looper > 1000) {
+  //   Serial.println(SELECTED_BTN);
+  //   looper = 0;
+  // }
+  
+
+  // control servo motors
+  if (BTN_TOP_FLAG) {
+    leftServo.writeSpeed(-SERVO_SPEED);
+    rightServo.writeSpeed(SERVO_SPEED);
+  }
+  else if (BTN_BOTTOM_FLAG) {
+    leftServo.writeSpeed(SERVO_SPEED);
+    rightServo.writeSpeed(-SERVO_SPEED);
+  }
+  else if (BTN_LEFT_FLAG) {
+    leftServo.writeSpeed(-SERVO_SPEED);
+    rightServo.writeSpeed(-SERVO_SPEED);
+  }
+  else if (BTN_RIGHT_FLAG) {
+    leftServo.writeSpeed(SERVO_SPEED);
+    rightServo.writeSpeed(SERVO_SPEED);
+  }
+  else {
+    leftServo.writeSpeed(0);
+    rightServo.writeSpeed(0);
+  }
+
 }
--- hardware/slave/slave.ino
+++ hardware/slave/slave.ino
@@ -12,6 +12,12 @@ const char *password = APPSK;
 
 AsyncWebServer server(80);
 
+void spam(uint8_t outByte) {
+  for(int i = 0; i < 10; i++) {
+    Serial.write(outByte);
+  }
+}
+
 void handleRoot(AsyncWebServerRequest *request) {
   request->send(200, "text/html", "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0'><title>Controller</title><style>body{margin:0;padding:0}.mark-red{background-color:rgba(255,0,0,.1);border:1px solid red}.mark-cyan{background-color:rgba(0,255,255,.1);border:1px solid #0ff}.unselectable{-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.buttons-wrapper{position:absolute;width:90%;height:auto;bottom:20px;left:0;right:0;margin:auto}.button,.wrapper{width:100%;height:100%}.box{width:100%;position:relative}.box:before{content:'';display:block;padding-top:100%}.box .content{position:absolute;top:0;left:0;bottom:0;right:0;display:grid;grid-template-columns:33% 33% 33%;grid-template-rows:33% 33% 33%}.wrapper{padding:5%;box-sizing:border-box}.button{border-radius:12%;background-color:#7a8ca0;color:#fff;font-family:Arial;font-size:600%;line-height:92%;text-align:center}.active{background-color:#363a3a}.btn-left{grid-area:2/1/2/1;transform:rotate(-90deg)}.btn-right{grid-area:2/3/2/3;transform:rotate(90deg)}.btn-bottom{grid-area:3/2/3/2;transform:rotate(180deg)}.btn-top{grid-area:1/2/1/2}</style><script type='application/javascript'>function isTouchscreen(){return!!window.matchMedia('(pointer:coarse)').matches}function bind(t,e,o){var t=document.querySelector(t),n=isTouchscreen?'touchend':'mouseup';t.addEventListener(isTouchscreen?'touchstart':'mousedown',t=>{t.target.classList.add('active'),e()}),t.addEventListener(n,t=>{t.target.classList.remove('active'),o()})}document.addEventListener('DOMContentLoaded',()=>{bind('.btn-left .button',()=>{console.log('turn left'),fetch('/controller?btn=left&state=1')},()=>{console.log('stop'),fetch('/controller?btn=left&state=0')}),bind('.btn-right .button',()=>{fetch('/controller?btn=right&state=1')},()=>{fetch('/controller?btn=right&state=0')}),bind('.btn-top .button',()=>{fetch('/controller?btn=top&state=1')},()=>{fetch('/controller?btn=top&state=0')}),bind('.btn-bottom .button',()=>{fetch('/controller?btn=bottom&state=1')},()=>{fetch('/controller?btn=bottom&state=0')})});</script></head><body><div class='buttons-wrapper'><div class='box'><div class='content'><div class='wrapper btn-left'><div class='button unselectable'>&#8593;</div></div><div class='wrapper btn-right'><div class='button unselectable'>&#8593;</div></div><div class='wrapper btn-top'><div class='button unselectable'>&#8593;</div></div><div class='wrapper btn-bottom'><div class='button unselectable'>&#8593;</div></div></div></div></div></body></html>");
 }
@@ -21,20 +27,20 @@ void handleController(AsyncWebServerRequest *request) {
     String btn = request->getParam("btn")->value();
     bool state = request->getParam("state")->value() == "1";
     if(btn == "left") {
-      Serial.print(1);
-      Serial.println(state);
+      if (state) spam(1);
+      else spam(2);
     }
     if(btn == "right") {
-      Serial.print(2);
-      Serial.println(state);
+      if (state) spam(3);
+      else spam(4);
     }
     if(btn == "top") {
-      Serial.print(3);
-      Serial.println(state);
+      if (state) spam(5);
+      else spam(6);
     }
     if(btn == "bottom") {
-      Serial.print(4);
-      Serial.println(state);
+      if (state) spam(7);
+      else spam(8);
     }
     request->send(200, "application/json", "{'status': 'ok'}");
   }
