34e520e1e419fbcf407a937013b4e5b7b0ab1ab1
btxdev <limecompro@gmail.com>
2023-03-07 09:12:12 +0500

    Забить на классы и начать писать по-быстрому

--- hardware/mega/MyServo.cpp
+++
@@ -1,7 +0,0 @@
-#include "MyServo.h"
-
-uint8_t MyServo::attach(int pin, int minPulseWidth, int maxPulseWidth,
-                             int minAngle, int maxAngle) {
-    _summaryAngle = 0;
-    AmperkaServo::attach(pin, minPulseWidth, maxPulseWidth, minAngle, maxAngle);
-}
